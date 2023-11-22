9f78bb17f6a97331bc1397f972b29590f1a06517
Gopi Krishna Suvanam <68820170+gopi-suvanam@users.noreply.github.com>
2023-11-22 11:29:10 +0530

    Update dsp.js

--- dsp.js
+++ dsp.js
@@ -2374,7 +2374,7 @@ Array.prototype.synthesis=function(sample_rate,segment_size,hop_size){
     const num_segments=this.length;
     for (let i = 0; i < num_segments; i++) {
       const fft = new FFT(segment_size, sample_rate);
-      const segment = fft.inverse(this[i));
+      const segment = fft.inverse(this[i]);
       buffer=buffer.concst(segment);
     }
   return buffer;
